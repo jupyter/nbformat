2380ddf7e9c12285e03887a4dac26f6c2666d184
Min RK <benjaminrk@gmail.com>
2015-04-08 18:10:22 -0700

    add package skeleton

---
+++ setup.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+from __future__ import print_function
+
+# the name of the project
+name = 'jupyter_nbformat'
+
+#-----------------------------------------------------------------------------
+# Minimal Python version sanity check
+#-----------------------------------------------------------------------------
+
+import sys
+
+v = sys.version_info
+if v[:2] < (2,7) or (v[0] >= 3 and v[:2] < (3,3)):
+    error = "ERROR: %s requires Python version 2.7 or 3.3 or above." % name
+    print(error, file=sys.stderr)
+    sys.exit(1)
+
+PY3 = (sys.version_info[0] >= 3)
+
+#-----------------------------------------------------------------------------
+# get on with it
+#-----------------------------------------------------------------------------
+
+import os
+from glob import glob
+
+from distutils.core import setup
+
+pjoin = os.path.join
+here = os.path.abspath(os.path.dirname(__file__))
+pkg_root = pjoin(here, name)
+
+packages = []
+for d, _, _ in os.walk(pjoin(here, name)):
+    if os.path.exists(pjoin(d, '__init__.py')):
+        packages.append(d[len(here)+1:].replace(os.path.sep, '.'))
+
+version_ns = {}
+with open(pjoin(here, name, '_version.py')) as f:
+    exec(f.read(), {}, version_ns)
+
+
+setup_args = dict(
+    name            = name,
+    version         = version_ns['__version__'],
+    scripts         = glob(pjoin('scripts', '*')),
+    packages        = packages,
+    description     = "The Jupyter Notebook format",
+    long_description= """
+    This package contains the base implementation of the Jupyter Notebook format,
+    and Python APIs for working with notebooks.
+    """,
+    author          = 'Jupyter Development Team',
+    author_email    = 'jupyter@googlegroups.com',
+    url             = 'http://jupyter.org',
+    license         = 'BSD',
+    platforms       = "Linux, Mac OS X, Windows",
+    keywords        = ['Interactive', 'Interpreter', 'Shell', 'Web'],
+    classifiers     = [
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+)
+
+if 'develop' in sys.argv or any(a.startswith('bdist') for a in sys.argv):
+    import setuptools
+
+setuptools_args = {}
+install_requires = setuptools_args['install_requires'] = [
+    'ipython_genutils',
+    'traitlets',
+    'jsonschema>=2.0',
+]
+
+extras_require = setuptools_args['extras_require'] = {
+}
+
+if 'setuptools' in sys.modules:
+    setup_args.update(setuptools_args)
+
+if __name__ == '__main__':
+    setup(**setup_args)
