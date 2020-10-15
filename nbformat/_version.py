78d0565a4a0ad2e1f08f7390841d5d66c46deeb2
goanpeca <goanpeca@gmail.com>
2020-10-14 19:31:27 -0500

    Add CI workflow for pakcage release on tag push

--- nbformat/_version.py
+++ nbformat/_version.py
@@ -1,3 +1,3 @@
 # Make sure to update package.json, too!
-version_info = (5, 0, 9, 'dev')
+version_info = (5, 0, 9, 'dev0')
 __version__ = '.'.join(map(str, version_info))
--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ install_requires = setuptools_args['install_requires'] = [
 
 extras_require = setuptools_args['extras_require'] = {
     'fast': ['fastjsonschema'],
-    'test': ['fastjsonschema', 'testpath', 'pytest', 'pytest-cov'],
+    'test': ['check-manifest', 'fastjsonschema', 'testpath', 'pytest', 'pytest-cov'],
 }
 
 if 'setuptools' in sys.modules:
