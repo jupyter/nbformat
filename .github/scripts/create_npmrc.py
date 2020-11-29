e9cdc35f26bdc38dfeb0b2a9c66174b66b2403b5
goanpeca <goanpeca@gmail.com>
2020-11-29 17:17:39 -0500

    Add scripts and handling of environment variables

---
+++ .github/scripts/create_npmrc.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+# Standard library imports
+import os
+
+
+def create_npmrc():
+    """
+    Create NPM configuration file in user home directory to use authentication
+    token from environment variables.
+    """
+    fpath = os.path.expanduser("~/.npmrc")
+    with open(fpath, "w") as fh:
+        fh.write("//registry.npmjs.org/:_authToken=${NPM_TOKEN}")
+
+
+if __name__ == "__main__":
+    create_npmrc()
