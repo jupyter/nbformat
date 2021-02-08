3e2d91c3c98b9a3072683e1f4e206be2c2615679
Marc Wouts <marc.wouts@gmail.com>
2021-02-08 19:06:29 +0100

    Store words in a list (rather than a set) to avoid deprecation warnings when sampling #212

--- nbformat/corpus/words.py
+++ nbformat/corpus/words.py
@@ -7,13 +7,13 @@ from functools import lru_cache
 @lru_cache(1)
 def acceptable_nouns():
     with open(os.path.join(os.path.dirname(__file__), 'nouns.txt'), 'r') as nouns_file:
-        return set(word.rstrip() for word in nouns_file.readlines())
+        return list(word.rstrip() for word in nouns_file.readlines())
 
 
 @lru_cache(1)
 def acceptable_adjectives():
     with open(os.path.join(os.path.dirname(__file__), 'adjectives.txt'), 'r') as adjectives_file:
-        return set(word.rstrip() for word in adjectives_file.readlines())
+        return list(word.rstrip() for word in adjectives_file.readlines())
 
 
 def generate_corpus_id():
