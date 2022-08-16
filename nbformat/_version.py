e204b5670698c3690e3d4ff5aafcf91730926c34
Matthias Bussonnier <bussonniermatthias@gmail.com>
2022-08-16 14:46:24 +0200

    just ignore with mypy

--- nbformat/_version.py
+++ nbformat/_version.py
@@ -4,6 +4,6 @@ version_info = (5, 4, 0)
 if len(version_info) <= 3:
     version_extra = ""
 else:
-    version_extra = version_info[3]
+    version_extra = version_info[3]  # type: ignore
 
 __version__ = ".".join(map(str, version_info[:3])) + version_extra
