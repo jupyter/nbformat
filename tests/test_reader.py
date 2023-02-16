b26bc1a44a75446557a3f9c7b0f8965047ecb69b
Csaba Drahos <csaba.drahos@gmail.com>
2023-02-16 19:43:41 +0100

    replace references from COPYING.md (and COPYING) to LICENSE

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -5,7 +5,7 @@ Contains tests class for reader.py
 #  Copyright (C) 2013  The IPython Development Team
 #
 #  Distributed under the terms of the BSD License.  The full license is in
-#  the file COPYING, distributed as part of this software.
+#  the file LICENSE, distributed as part of this software.
 # -----------------------------------------------------------------------------
 
 # -----------------------------------------------------------------------------
