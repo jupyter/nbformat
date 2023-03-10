34fb7fc87e8f5244b481bd30f068ad61580a6f6e
Steven Silvester <steven.silvester@ieee.org>
2023-03-09 19:19:58 -0600

    lint

--- nbformat/validator.py
+++ nbformat/validator.py
@@ -25,9 +25,9 @@ def _relax_additional_properties(obj):
     """relax any `additionalProperties`"""
     if isinstance(obj, dict):
         for key, value in obj.items():
-            value = (
+            value = (  # noqa
                 True if key == "additionalProperties" else _relax_additional_properties(value)
-            )  # noqa
+            ) 
             obj[key] = value
     elif isinstance(obj, list):
         for i, value in enumerate(obj):
